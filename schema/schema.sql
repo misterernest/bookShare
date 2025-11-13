a79923b51bd5d5027e99baec04c9e4fae2b50ed1
Ernesto Campos Villa <misterernest@hotmail.com>
2025-11-14 09:31:05 +1100

    add tables book_score and user_books to schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -34,3 +34,23 @@ create table if not exists club_members (
         on update current_timestamp,
     unique key no_rep(user_id, club_id)
 ) Engine=InnoDB default charset=utf8mb4 collate=utf8mb4_bin;
+
+create table if not exists user_books (
+    user_books_is integer unsigned primary key auto_increment,
+    user_id integer not null,
+    book_id integer not null,
+    created_at timestamp not null default current_timestamp,
+    modified_at timestamp not null default current_timestamp 
+        on update current_timestamp,
+    unique key no_rep(user_id, book_id)
+) Engine=InnoDB default charset=utf8mb4 collate=utf8mb4_bin;
+
+create table if not exists book_scores (
+    book_score_id integer unsigned primary key auto_increment,
+    user_id integer unsigned not null,
+    book_id integer unsigned not null,
+    score tinyint unsigned,
+    created_at timestamp not null default current_timestamp,
+    modified_at timestamp not null default current_timestamp on update current_timestamp,
+    unique key no_rep(user_id, book_id)
+) Engine=InnoDB default charset=utf8mb4 collate=utf8mb4_bin;
