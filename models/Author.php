cc7e0136e39827910ffee5640f0c178a630fac31
Ernesto Campos Villa <misterernest@hotmail.com>
2025-11-18 18:43:26 +1100

    add author model with hasMany

--- models/Author.php
+++ models/Author.php
@@ -14,4 +14,14 @@ class Author extends ActiveRecord
     public function getId() {
         return $this->author_id;
     }
+
+    public function toString() {
+        return sprintf("%s (%s)", $this->name, count($this->books));
+    }
+
+    public function getBooks() {
+        return $this
+            ->hasMany(Book::class, ['author_id' => 'author_id'])
+            ->all();
+        }
 }
