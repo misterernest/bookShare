9b7afd9e1070c7479efc666dd217ed2e27a4130d
Ernesto Campos Villa <misterernest@hotmail.com>
2025-12-01 15:51:50 +1100

    add author controller

---
+++ controllers/AuthorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\models\Author;
+
+
+class AuthorController extends Controller
+{
+    public function actionAll($search = null) {
+        if ($search != null) {
+            $authors = Author::find()
+                ->where(['like', 'name', $search])
+                ->all();
+            return serialize($authors);
+        }
+
+        $authors = Author::find()->all();
+        return serialize($authors);
+    }
+
+    public function actionDetail($id) {
+
+        $author = Author::findOne($id);
+        if (empty($author)) {
+            // return $this->redirect(['site/index']);
+            Yii::$app->session->setFlash('warning', 'Author not found.');
+            return $this->redirect(['author/all']);
+        }
+
+        return $author->toString();
+    }
+}
