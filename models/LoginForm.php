205e498cb906b3c77022a69518303939eb3bb219
Ernesto Campos Villa <misterernest@hotmail.com>
2025-12-02 11:54:21 +1100

    add error login to form

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -48,6 +48,7 @@ class LoginForm extends Model
             $user = $this->getUser();
 
             if (!$user || !$user->validatePassword($this->password)) {
+                Yii::$app->session->setFlash("error", "Incorrect username or password.");
                 $this->addError($attribute, 'Incorrect username or password.');
             }
         }
