6ceac134bbe0ebbb471fc8ed59273e43e5be8afa
Ernesto Campos Villa <misterernest@hotmail.com>
2026-01-27 17:18:02 +1100

    add new book from controller

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -25,4 +25,26 @@ class BookController extends Controller
 
         return $book->toString();
     }
+
+    public function actionNew() {
+        if (Yii::$app->user->isGuest) {
+            //only logged in users should be able to create new books
+            return $this->goHome();
+        }
+
+        $book = new Book();
+        if ($book->load(Yii::$app->request->post())) {
+            //there are some data on post for me and we need to validate and save
+            if ($book->validate()) {
+                if ($book->save()) {
+                    Yii::$app->session->setFlash('success', 'Book created successfully.');
+                    return $this->redirect(['book/all']);
+                } else {
+                    throw new \Exception('Failed to save book.');
+                    return;
+                }
+            }
+        }
+        return $this->render('form.tpl', ['book' => $book]);
+    }
 }
