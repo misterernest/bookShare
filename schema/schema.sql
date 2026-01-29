23f68f02de7bc04d140279aca93257f41fdbdf0b
Ernesto Campos Villa <misterernest@hotmail.com>
2026-01-29 12:51:10 +1100

    add model userBook and table for stand

--- schema/schema.sql
+++ schema/schema.sql
@@ -63,3 +63,16 @@ create table if not exists book_scores (
     modified_at timestamp not null default current_timestamp on update current_timestamp,
     unique key no_rep(user_id, book_id)
 ) Engine=InnoDB default charset=utf8mb4 collate=utf8mb4_bin;
+
+
+////////////////////////
+
+
+create table if not exists user_books (
+    user_books_id integer unsigned primary key auto_increment,
+    user_id integer unsigned not null,
+    book_id integer unsigned not null,
+    created_at timestamp not null default current_timestamp,
+    modified_at timestamp not null default current_timestamp on update current_timestamp,
+    unique key no_rep(user_id, book_id)
+) Engine=InnoDB default charset=utf8mb4 collate=utf8mb4_bin;
